438240ad200485f9c7b70126b4df0f0fc873b306
Amber <alammers83@users.noreply.github.com>
2020-01-21 20:25:50 -0500

    Update userexportTemplate.py

--- userexportTemplate.py
+++ userexportTemplate.py
@@ -7,9 +7,9 @@ url = "https://gitlab.com/api/v4/groups/6543/members/all"
 querystring = {"per_page":"100","page":"1"}
 
 headers = {
-    'Authorization': "Bearer hJ1rcE_y1DWJBtZxU2tK",
+    'Authorization': "Bearer token",
     'cache-control': "no-cache",
-    'Postman-Token': "personal-token"
+    'Postman-Token': "postman-token"
     }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
